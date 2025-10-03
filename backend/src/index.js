b1a7ff34fea5cb6d8ce2d16bec87cbbde63ab8fb
Rohan Jacob <123861149+jakereacher@users.noreply.github.com>
2025-10-03 18:41:35 +0530

    completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,9 +8,13 @@ import messageRoutes from "./routes/message.route.js"
 import { app, server } from "./lib/socket.js";
 import cors from "cors"
 
+
+import path from "path";
+
 dotenv.config()
 
 const PORT = process.env.PORT
+const __dirname = path.resolve();
 
 
 app.use(cors({
@@ -28,6 +32,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 
 server.listen(PORT, () => {

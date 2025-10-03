fbd665158410719ff452cda1bcb8d3ab2f37d465
Rohan Jacob <123861149+jakereacher@users.noreply.github.com>
2025-10-03 18:58:58 +0530

    Deployment tests done

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,47 +1,42 @@
 import express from "express";
-import dotenv from "dotenv"
-import cookieParser from "cookie-parser"
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 
 import { connectDB } from "./lib/db.js";
-import authRoutes from "./routes/auth.route.js"
-import messageRoutes from "./routes/message.route.js"
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
-import cors from "cors"
-
+import cors from "cors";
 
 import path from "path";
 
-dotenv.config()
+dotenv.config();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-
 app.use(cors({
-  origin:"http://localhost:5173",
-  credentials:true,
-}
-))
-
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
-app.use(cookieParser())
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
-
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
+  // Use regexp to exclude /api routes from frontend fallback
+  app.get(/^\/(?!api).*/, (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
-
 server.listen(PORT, () => {
   console.log("app is running on port " + PORT);
-  connectDB()
+  connectDB();
 });
